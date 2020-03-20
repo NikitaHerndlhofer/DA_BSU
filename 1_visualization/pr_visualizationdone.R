7c98b798cce54cf4693a44b8557b13db36d799a6
Akber_Khalilov <akber.khaliov@gmail.com>
2020-03-20 15:38:22 +0000

    hw

--- 1_visualization/pr_visualizationdone.R
+++ 1_visualization/pr_visualizationdone.R
@@ -5,11 +5,17 @@
 # For each variable see meaning, type and values. 
 
 # Exersice 1. For any of 5 timeseries (pce, pop, psavert, uempmed, unemploy) make plots (x = date) using geoms point, line and smooth.
-
+ggplot(data = economics)+
+  geom_point(mapping = aes(x = date, y = psavert))
+ggplot(data = economics)+
+  geom_line(mapping = aes(x = date, y = psavert))
+ggplot(data = economics)+
+  geom_smooth(mapping = aes(x=date, y=psavert))
 # Exersice 2. Display pce and psavert on one plot.
-
+ggplot(data = economics, aes(x =date, y =psavert))+ geom_line()
 # Exersice 3. Display three variables on one scatterplot (x = date, y = unemploy, size = uempmed).
-
+ggplot(data = economics) + 
+  geom_point(mapping = aes(x = date, y = unemploy, size = uempmed))
 # Exersice 4. Run the following code line by line and explain what it does.
 
 economics$date
@@ -26,16 +32,33 @@ my_economics$decade <- as.factor(my_economics$decade)
 class(my_economics$decade)
 
 # Exersice 5. Plot for x = uempmed, y = unemploy facet_wrap on decade.
+ggplot(data = economics)+
+  geom_point(mapping = aes(x = uempmed, y = unemploy))+
+  facet_wrap(~my_economics$decade)
 
 # Exersice 6. On one plot (x = uempmed, y = unemploy) display a separate geom_smoth object for each decade.
-
+ggplot(data = economics)+
+  geom_smooth(mapping = aes(x = uempmed, y = unemploy))+
+  facet_wrap(~my_economics$decade)
 # Exersice 7. Plot histogram for unemploy for each decade.
+ggplot(economics,aes(unemploy))+
+  geom_histogram()+
+  facet_wrap(~my_economics$decade)
 
 # Exersice 8. Plot geom_boxplot and stat_summary for unemploy for each decade.
+ggplot(economics, aes(unemploy))+
+  geom_boxplot()+
+  facet_wrap(~my_economics$decade)
 
+ggplot(economics)+
+  stat_summary(
+    mapping = aes(x = my_economics$decade, y = unemploy),
+    fun.xmin = min,
+    fun.xmax = max,
+    fun.x = median
+  )
+  
 # Exersice 9. Set working directory and save one of the plots to it
 
 # Exersice 10. Compose 3 questions (in Russian and English) on the topic 'visualisation' with 3-4 variants of answers only 1 of wich is correct
-
-
 
