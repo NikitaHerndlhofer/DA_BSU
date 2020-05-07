b63bf5806a0d3f2a7f2d47fc82062fbf28df0af4
dimaraspopov <you@example.com>
2020-05-07 00:29:48 +0000

    Update

---
+++ 6_clustering/code/pr_clustering_done.R
@@ -0,0 +1,144 @@
+# 1) Make a Scree Plot for school_result dataset with ggplot()
+
+set.seed(42)
+
+school_result <- read.csv("/cloud/project/6_clustering/data/school_result.csv", stringsAsFactors=FALSE)
+
+ratio_ss <- rep(0, 7)
+
+# Finish the for-loop
+for (k in 1:7) {
+  
+# Apply k-means to school_result: school_km
+school_km <- kmeans(school_result, k, nstart = 20)
+
+# Save the ratio between of WSS to TSS in kth element of ratio_ss
+ratio_ss[k] <- school_km$tot.withinss / school_km$totss
+  
+}
+
+df = data.frame(k=seq(1,7), ratio_ss)
+
+plot(ratio_ss, type = "b", xlab = "k")
+
+ggplot(df, aes(x=k, y=ratio_ss)) +
+  geom_line() +
+  geom_point()
+ggsave("img.png")
+
+# 2) Perform Complete Hierarchical Clustering for run_record_sc
+
+# Modify code for single-linkage (below) to complete linkage (hclust with method = "complete")
+# Code for single-linkage
+
+run_dist <- dist(run_record_sc, method = "euclidean")
+run_single <- hclust(run_dist, method = "single")
+memb_single <- cutree(run_single, 5)
+plot(run_single)
+rect.hclust(run_single, k = 5, border = 2:6)
+
+
+# Code for complete-linkage
+
+run_complete <- hclust(run_dist, method = "complete")
+memb_complete <- cutree(run_complete, k = 5)
+plot(run_complete)
+rect.hclust(run_complete, k = 5, border = 2:6)
+
+
+# 3) Compare obtained plot with one for single-linkage (see clustering.R) and make a conclusion about clusters obtained via single- and the complete-linkage clustering.
+
+# In single-linkage: the similarity of their most similar members, clusters with the smallest minimum pairwise distance.
+# In complete-linkage: the similarity of their most dissimilar members, clusters with the smallest maximum pairwise distance.
+
+# 4) Compare the membership between the single and the complete linkage clusterings for run_record_sc, using table().
+
+table(memb_single, memb_complete)
+
+# 5) Hierarchical vs k-means
+# So you've clustered the countries based on their Olympic run performances using three different methods: 
+# k-means clustering, hierarchical clustering with single linkage and hierarchical clustering with complete linkage. 
+# You can ask yourself: which method returns the best separated and the most compact clusters?
+
+# Let's use Dunn's index. Remember, it returns the ratio between the minimum intercluster distance to the maximum intracluster diameter. 
+# The dunn() function in R, requires the argument clusters, indicating the cluster partitioning, the Data and a method to determine the distance. 
+# In this case, that's "euclidean", which is the default.
+
+# Your job is to calculate Dunn's index for all three clusterings and compare the clusters to each other. 
+# Set random seed. Don't remove this line.
+
+set.seed(42)
+
+# Dunn's index for k-means: dunn_km
+
+dunn_km <- dunn(clusters = run_km_sc$cluster, Data = run_record_sc)
+# Dunn's index for single-linkage: dunn_single
+dunn_single <- dunn(clusters = memb_single, Data = run_record_sc)
+
+# Dunn's index for complete-linkage: dunn_complete
+dunn_complete <- dunn(clusters = memb_complete, Data = run_record_sc)
+
+# Compare k-means with single-linkage
+table(run_km_sc$cluster, memb_single)
+
+# Compare k-means with complete-linkage
+table(run_km_sc$cluster, memb_complete)
+
+
+# 6) Which of the following statements about your clusterings of the countries in run_record_sc are correct? 
+# Possible Answers
+# 1) The complete-linkage method returned the lowest ratio of minimal intercluster-distance to minimal cluster diameter.
+# 2) Based on Dunn's index, the complete-linkage method returned the most compact and separated clusters.
+# 3) The single-linkage method returned the highest ratio of minimal intercluster-distance to maximal cluster diameter.
+# 4) Based on Dunn's index, the single-linkage method returned the least compact and separated clusters.
+
+# Correct answer is # 3) The single-linkage method returned the highest ratio of minimal intercluster-distance to maximal cluster diameter.
+
+
+
+# 7) Clustering US States Based on Criminal Activity
+# You've seen that different clustering methods can return entirely different clusters, each with their own interpretation and uses. 
+# It's time to put your skills, both the programming and the interpretation, to the test!
+
+# Your client has provided you with a dataset, crime_data (crime_data.csv in dir data), 
+# containing info on the crimes committed in each of the 50 US states and the percentage of urban population (Source: Edureka). 
+# He'd like you to group the states in 4 clusters. 
+# He didn't specify which similarity to use, but the euclidean distance seems acceptable, don't you agree?
+
+# You decide to try out two techniques: k-means and single-linkage hierarchical clustering. 
+# You then want to compare the results by calculating the Dunn's indices to make a conclusion. 
+# Which clustering will you deliver to your client?
+
+
+# Load the data
+crime_data <- read.csv("/cloud/project/6_clustering/data/crime_data.csv", stringsAsFactors=FALSE)
+
+# Scale the dataset: crime_data_sc
+crime_data_sc <- scale(crime_data)
+
+# Perform k-means clustering: crime_km
+crime_km <- kmeans(crime_data_sc, 5, nstart = 20)
+
+# Perform single-linkage hierarchical clustering
+## Calculate the distance matrix: dist_matrix
+dist_matrix <- dist(crime_data_sc)
+
+## Calculate the clusters using hclust(): crime_single
+crime_single <- hclust(dist_matrix, method = "single")
+
+## Cut the clusters using cutree: memb_single
+memb_single <- cutree(crime_single, 4)
+
+# Calculate the Dunn's index for both clusterings: dunn_km, dunn_single
+dunn_km <- dunn(clusters = crime_km$cluster, Data = crime_data_sc)
+dunn_single <- dunn(clusters = memb_single, Data = crime_data_sc)
+
+# Print out the results
+print(dunn_km) 
+# 0.1604403
+
+print(dunn_single) 
+# 0.2438734
+
+# Try to print out some of the variables you created and see whether they make sense. 
+# How do the techniques compare? What clustering do you offer to your client?
