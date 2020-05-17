662877790057eb0e81d7c3cfceada4ddb0d2e2ca
Roman-codder <62114291+Roman-codder@users.noreply.github.com>
2020-05-17 20:24:15 +0300

    Create pr_eda.r

---
+++ 3_EDA/pr_eda.r
@@ -0,0 +1,41 @@
+# Exersice 1. Base on diamonds df, explore the distribution of price, try different binwidth. Make a conclusion
+
+
+
+# Exersice 2. Base on diamonds df, explore the distribution of clarity and color
+
+
+
+# Exersice 3. How many diamonds in diamonds df are 0.99 carat? How many are 1 carat? 
+# What do you think is the cause of the difference?
+
+
+# Exersice 4. How many missing values are there in flights df?
+
+
+# Exersice 5. What is the mean time of departure delay according to flights$dep_delay?
+# What na.rm = TRUE does?
+
+
+
+# Exersice 6. Base on flights df, make new df with additional variables: dep_hour, dep_min, dep_time_formatted
+# Calculate dep_hour as dep_time %/% 100, dep_min as dep_time %% 100, dep_time_formatted as dep_hour + dep_min / 60
+# Analyze variance of dep_hour and dep_time_formatted. 
+# What happens with missing values (NA) in charts?
+
+
+
+# Exersice 7. Base on flights df, discover covariance between dep_delay and arr_delay, 
+# distance and arr_delay. Which charts are more demonstrative?
+
+
+
+# Exersice 8. Base on flights df, discover covariance between carrier and cancelled flights (hint: add variable cancelled flights)
+
+
+
+# Exersice 9. Base on flights df, discover covariance between carrier and dep_delay
+
+
+
+# Exersice 10. Make eda for mpg df
